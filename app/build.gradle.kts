e39ca1f47adcd45e03a0f3b78ba3f3686dc3a936
n9nik <nikhil.suresh@gmail.com>
2026-09-09 21:21:50 -0700

    Fix build: add material-icons-extended for PhotoLibrary/PhotoCamera icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,6 +114,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.exifinterface:exifinterface:1.4.1")
 
     // CameraX for document capture
